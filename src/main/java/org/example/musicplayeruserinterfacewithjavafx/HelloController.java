674a329cef1ed9fe71ad1fc36378bfaa1a05b3a6
Kosmas <kosmaspanserraikos@gmail.com>
2024-11-13 17:41:08 +0200

    Images In Top Charts

--- src/main/java/org/example/musicplayeruserinterfacewithjavafx/HelloController.java
+++ src/main/java/org/example/musicplayeruserinterfacewithjavafx/HelloController.java
@@ -28,25 +28,37 @@ public class HelloController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        recentlyPlayed= new ArrayList<>(getRecentlyPlayed());
+        recentlyPlayed = new ArrayList<>(getRecentlyPlayed());
+        favorites=new ArrayList<>(getFavorites());
         try {
-            for(Song song : recentlyPlayed){
-                FXMLLoader fxmlLoader =new FXMLLoader();
+            for (Song song : recentlyPlayed) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("song.fxml"));
 
                 VBox vBox = fxmlLoader.load();
-                SongController songController =fxmlLoader.getController();
+                SongController songController = fxmlLoader.getController();
                 songController.setData(song);
 
                 recentlyPlayedContainer.getChildren().add(vBox);
             }
+            for (Song song : favorites) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("song.fxml"));
 
-        }catch (IOException e) {
+                VBox vBox = fxmlLoader.load();
+                SongController songController = fxmlLoader.getController();
+                songController.setData(song);
+
+                favoriteContainer.getChildren().add(vBox);
+            }
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-    private List<Song> getRecentlyPlayed(){
+
+    private List<Song> getRecentlyPlayed() {
         List<Song> ls = new ArrayList<>();
 
         Song song = new Song();
@@ -87,6 +99,39 @@ public class HelloController implements Initializable {
         ls.add(song);
 
 
+        return ls;
+    }
+
+    public List<Song> getFavorites() {
+        List<Song> ls = new ArrayList<>();
+        Song song = new Song();
+        song.setName("Top 50");
+        song.setArtist("Global");
+        song.setCover("/img/Top50Charts.png");
+        ls.add(song);
+
+        song = new Song();
+        song.setName("Top 50");
+        song.setArtist("USA");
+        song.setCover("/img/TopUsa.png");
+        ls.add(song);
+
+        song = new Song();
+        song.setName("Top 50");
+        song.setArtist("Italy");
+        song.setCover("/img/TopItaly.png");
+        ls.add(song);
+
+        song = new Song();
+        song.setName("Top 50");
+        song.setArtist("Viral Global");
+        song.setCover("/img/ViralGlobal.png");
+        ls.add(song);
+        song = new Song();
+        song.setName("Top 50");
+        song.setArtist("Greece");
+        song.setCover("/img/TopGreece.png");
+        ls.add(song);
 
         return ls;
     }
