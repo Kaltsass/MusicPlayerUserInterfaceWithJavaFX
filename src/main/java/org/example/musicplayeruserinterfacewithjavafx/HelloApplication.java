6c44e76f46bb315833df629c2b9223cfd1b2c031
Aggelos <langelo2001@outlook.com>
2024-11-22 14:52:41 +0200

    Minor fixes
    Minor fixes in the gradle build so sdk
    works with gradle and javafx properly

--- src/main/java/org/example/musicplayeruserinterfacewithjavafx/HelloApplication.java
+++ src/main/java/org/example/musicplayeruserinterfacewithjavafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1315, 890);
+        Scene scene = new Scene(fxmlLoader.load(), 1800, 980);
         Objects.requireNonNull(getClass().getClassLoader().getResource("css/style.css")).toExternalForm();
         stage.setTitle("Music Player");
         stage.setScene(scene);
