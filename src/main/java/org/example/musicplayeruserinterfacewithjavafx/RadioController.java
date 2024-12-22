836e833799f8f032b5ed8d45129db00c1ad83ce0
Rakip Lt <licirakip03@gmail.com>
2024-12-22 13:38:55 +0200

    Rakip10_Radio4

--- src/main/java/org/example/musicplayeruserinterfacewithjavafx/RadioController.java
+++ src/main/java/org/example/musicplayeruserinterfacewithjavafx/RadioController.java
@@ -6,11 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
-
 import javafx.scene.image.ImageView;
-
 import javafx.scene.layout.AnchorPane;
-
 import javafx.stage.Modality;
 
 import javafx.stage.Stage;
